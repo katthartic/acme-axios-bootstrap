70bb3f6d2fdeafb20b7363b1d19e3644e01aab47
katthartic <katt.baum@gmail.com>
2020-02-05 00:56:24 -0500

    Add renderNav and start renderTable

--- app.js
+++ app.js
@@ -1,6 +1,41 @@
-const URL = 'https://acme-users-api-rev.herokuapp.com/api/'
+const API = 'https://acme-users-api-rev.herokuapp.com/api/'
 
-Promise.all([axios.get(`${URL}companies`), axios.get(`${URL}products`)])
-    .then(response => response.map(result => result.data))
-    .then(console.log)
+const dataPromise = Promise.all([axios.get(`${API}companies`), axios.get(`${API}products`)])
+    .then(responses => {
+        return {Companies: responses[0].data, Products: responses[1].data}
+    })
+
+
+dataPromise.then(result => {
+    window.location.hash = '#Companies'
+    renderNav(result)
+    renderTable(result)
+})
+
+// const hash = window.location.hash.slice(1)
+
+function renderNav (data) {
+    const hash = window.location.hash.slice(1)
+    let navTabs = Object.entries(data).map(entry => {
+        [tabName, tabList] = entry
+        return `
+        <li class="nav-item ${hash === tabName ? 'active' : ''}">
+            <a class="nav-link" href="#${tabName}">${tabName} (${tabList.length})</a>
+        </li>`
+    }).join('')
+
+    document.querySelector('ul').innerHTML = navTabs
+}
+
+function renderTable (data) {
+    const hash = window.location.hash.slice(1)
+    let tableData = data[hash]
+
+    let header = Object.keys(tableData[0]).map(key => {
+        return `<th>${key}</th>`
+    }).join('')
+
+    console.log(header)
+    
+}
 
